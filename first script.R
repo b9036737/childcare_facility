b20a083b6316fb1215c5ad0156b7c01e4b0efd98
b9036737 <b9036737@my.shu.ac.uk>
2020-04-11 13:57:10 +0100

    first commit

---
+++ first script.R
@@ -0,0 +1,7 @@
+facilities <- read.xlsx("C:\\Users\\Ralst\\Documents\\1-UNI\\ADM\\Childcare Facility\\Datasets\\facilities.xlsx")
+housing <- read.xlsx("C:\\Users\\Ralst\\Documents\\1-UNI\\ADM\\Childcare Facility\\Datasets\\housing.xlsx")
+population <- read.xlsx("C:\\Users\\Ralst\\Documents\\1-UNI\\ADM\\Childcare Facility\\Datasets\\population.xlsx")
+
+library(tidyverse)
+library(openxlsx)
+
